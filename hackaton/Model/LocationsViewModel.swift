ca7ac905677c09bf7471c2124098c503eee03195
Ruben <ruben.suarezdz@udlap.mx>
2024-03-06 01:25:08 -0600

    Added map annotationview

--- hackaton/Model/LocationsViewModel.swift
+++ hackaton/Model/LocationsViewModel.swift
@@ -55,4 +55,28 @@ class LocationsViewModel: ObservableObject{
             showLocationsList = false
         }
     }
+    
+    func nextButtonPressed(){
+        //Get the current index
+
+        guard let currentIndex = locations.firstIndex(where: {$0 == mapLocation}) else {
+            print("Could not find current index in locations array! Should never happen.")
+            return
+        }
+        
+        //Next index is valid?
+        let nextIndex = currentIndex + 1
+        guard locations.indices.contains(nextIndex) else {
+            //Next index is not valid
+            //restart from 0
+            guard let firstLocation = locations.first else {return}
+            showNextLocation(location: firstLocation)
+            return
+        }
+        
+        // next index is valid
+        let nextLocation = locations[nextIndex]
+        showNextLocation(location: nextLocation)
+        
+    }
 }
---
+++ hackaton/View/MapViews/LocationMapAnnotationView.swift
@@ -0,0 +1,38 @@
+//
+//  LocationMapAnnotationView.swift
+//  hackaton
+//
+//  Created by José Ángel del Monte Salazar on 06/03/24.
+//
+
+import SwiftUI
+
+struct LocationMapAnnotationView: View {
+    let accentColor = Color("AccentColor")
+    var body: some View {
+        VStack(spacing: 0) {
+            Image(systemName: "leaf.circle")
+                .resizable()
+                .scaledToFit()
+                .frame(width: 30, height: 30)
+                .font(.headline)
+                .foregroundColor(.white)
+                .padding(6)
+                .background(.red)
+                .clipShape(Circle())
+            
+            Image(systemName: "triangle.fill")
+                .resizable()
+                .scaledToFit()
+                .foregroundColor(.red)
+                .frame(width: 10, height: 10)
+                .rotationEffect(Angle(degrees: 180))
+                .offset(y: -3)
+                .padding(.bottom, 40)
+        }
+    }
+}
+
+#Preview {
+    LocationMapAnnotationView()
+}
